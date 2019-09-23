d58e652e3eb8dea7e0a5c7905664ef393974cfb0
Kazumasa Shimomura <kazu.devapp@gmail.com>
2019-09-24 01:01:42 +0900

    Show hightlightView at got pitch motion

--- uikit/ui-kit/Scenes/Zenly/Views/ZenlyCardView.swift
+++ uikit/ui-kit/Scenes/Zenly/Views/ZenlyCardView.swift
@@ -1,6 +1,21 @@
 import UIKit
+import CoreMotion
 
 final class ZenlyCardView: UIView {
+    private var initialXAxis: Double?
+    private let motionManager: CMMotionManager = {
+        let manager = CMMotionManager()
+        return manager
+    }()
+
+    private let highlightView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+
+    private lazy var highlightViewTopConstraint = highlightView.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 0)
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
@@ -15,5 +30,34 @@ final class ZenlyCardView: UIView {
         layer.masksToBounds = true
         layer.cornerRadius = 16
         backgroundColor = .blue
+
+        highlightView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(highlightView)
+        NSLayoutConstraint.activate([
+            highlightView.leftAnchor.constraint(equalTo: leftAnchor),
+            highlightView.rightAnchor.constraint(equalTo: rightAnchor),
+            highlightView.heightAnchor.constraint(equalToConstant: 10),
+            highlightViewTopConstraint
+        ])
+
+        motionManager.startDeviceMotionUpdates(to: .main) { [weak self] data, error in
+            guard let data = data else {
+                if let error = error {
+                    print(error)
+                }
+                return
+            }
+            self?.update(xAxis: data.attitude.pitch)
+        }
+    }
+
+    private func update(xAxis rotation: Double) {
+        if initialXAxis == nil {
+            initialXAxis = rotation
+        }
+        guard let initialRotation = initialXAxis else { return }
+
+        print(rotation)
+        highlightViewTopConstraint.constant = frame.height * CGFloat(initialRotation - rotation)
     }
 }
