843cace63f6161650f967f9db3e98932f1eff28a
Kazumasa Shimomura <kazu.devapp@gmail.com>
2019-09-25 09:32:48 +0900

    Show highlight view behind star vie

--- uikit/ui-kit/Scenes/Zenly/Views/ZenlyCardView.swift
+++ uikit/ui-kit/Scenes/Zenly/Views/ZenlyCardView.swift
@@ -3,18 +3,19 @@ import CoreMotion
 
 final class ZenlyCardView: UIView {
     private var initialXAxis: Double?
+
     private let motionManager: CMMotionManager = {
         let manager = CMMotionManager()
         return manager
     }()
 
+    private var starAdded: Bool = false
+
     private let highlightView: UIView = {
         let view = UIView()
-        view.backgroundColor = .white
+        view.backgroundColor = UIColor.gray.withAlphaComponent(0.3)
         return view
     }()
-
-    private lazy var highlightViewTopConstraint = highlightView.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 0)
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -26,19 +27,53 @@ final class ZenlyCardView: UIView {
         configure()
     }
 
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        guard !starAdded else { return }
+
+        highlightView.frame = CGRect(x: 0, y: frame.midY, width: frame.width, height: 20)
+
+        var starPathes: [UIBezierPath] = []
+        while starPathes.count < 30 {
+            let (path, vertexes) = starVertexes(
+                radius: 20,
+                center: CGPoint(
+                    x: CGFloat(arc4random_uniform(UInt32(frame.width))),
+                    y: CGFloat(arc4random_uniform(UInt32(frame.height)))
+                )
+            )
+            var contains = false
+            for existsPath in starPathes {
+                guard !contains else { break }
+                contains = vertexes.compactMap { vertex in existsPath.contains(vertex) ? true : nil }.first ?? false
+            }
+            if !contains {
+                starPathes.append(path)
+            }
+        }
+
+        let starView = UIView(frame: bounds)
+        let starLayer = CAShapeLayer()
+        starLayer.frame = starView.bounds
+        starLayer.path = {
+            let path = UIBezierPath(rect: starLayer.bounds)
+            starPathes.forEach { path.append($0) }
+            path.usesEvenOddFillRule = true
+            return path.cgPath
+        }()
+        starLayer.fillColor = UIColor.systemBlue.cgColor
+        starLayer.fillRule = .evenOdd
+        starView.layer.addSublayer(starLayer)
+
+        addSubview(highlightView)
+        addSubview(starView)
+
+        starAdded = true
+    }
+
     private func configure() {
         layer.masksToBounds = true
         layer.cornerRadius = 16
-        backgroundColor = .systemBlue
-
-        highlightView.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(highlightView)
-        NSLayoutConstraint.activate([
-            highlightView.leftAnchor.constraint(equalTo: leftAnchor),
-            highlightView.rightAnchor.constraint(equalTo: rightAnchor),
-            highlightView.heightAnchor.constraint(equalToConstant: 20),
-            highlightViewTopConstraint
-        ])
 
         let xEffect = UIInterpolatingMotionEffect(keyPath: "center.x", type: .tiltAlongHorizontalAxis)
         xEffect.minimumRelativeValue = -30
@@ -62,40 +97,13 @@ final class ZenlyCardView: UIView {
         }
     }
 
-    override func draw(_ rect: CGRect) {
-        var starPathes: [UIBezierPath] = []
-
-        while starPathes.count < 30 {
-            let (path, vertexes) = starVertexes(
-                radius: 20,
-                center: CGPoint(
-                    x: CGFloat(arc4random_uniform(UInt32(rect.width))),
-                    y: CGFloat(arc4random_uniform(UInt32(rect.height)))
-                )
-            )
-            var contains = false
-            for existsPath in starPathes {
-                guard !contains else { break }
-                contains = vertexes.compactMap { vertex in existsPath.contains(vertex) ? true : nil }.first ?? false
-            }
-            if !contains {
-                starPathes.append(path)
-            }
-        }
-
-        UIColor.white.withAlphaComponent(0.3).setFill()
-        starPathes.forEach { path in
-            path.fill()
-        }
-    }
-
     private func update(xAxis rotation: Double) {
         if initialXAxis == nil {
             initialXAxis = rotation
         }
         guard let initialRotation = initialXAxis else { return }
 
-        highlightViewTopConstraint.constant = frame.height * CGFloat(initialRotation - rotation)
+        highlightView.center.y = center.y / 2 + center.y * CGFloat(initialRotation - rotation)
     }
 
     /// 星型正多角形の頂点
